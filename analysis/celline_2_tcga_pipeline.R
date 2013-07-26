cef3005263bbeca3cf648ea20034bb8911d6b468
jguinney <jguinney@belltown.(none)>
2013-07-26 15:18:06 -0700

    Randomization of features in VDS pipeline

--- analysis/celline_2_tcga_pipeline.R
+++ analysis/celline_2_tcga_pipeline.R
@@ -23,6 +23,9 @@ patient.ids <- function(x){
 }
 
 
+###################
+## builds dataset for each TCGA disease type; separate data for each profile type (i.e. gene expression, rppa, CNA)
+
 build.tcga.ds <- function(geneExprId, rppaId=NULL, gisticId=NULL, cbioPrefix, isRNASeq=TRUE, missenseFilter=TRUE){
   
   if(startsWith(geneExprId,"syn")){
@@ -83,7 +86,8 @@ build.tcga.ds <- function(geneExprId, rppaId=NULL, gisticId=NULL, cbioPrefix, is
 
 ####################################
 
-virtual_ic50 <- function(cellLineEset, drugName, testExprs, seed=2013, reverseDrug=FALSE,perf.eval=TRUE,num.bootstraps=20){
+virtual_ic50 <- function(cellLineEset, drugName, testExprs, seed=2013, reverseDrug=FALSE,perf.eval=TRUE,
+                         num.bootstraps=20,ncores=5, diseaseUpweight=NULL, diseaseOffset=FALSE,alpha=.1){
   #browser()
   if(!is.list(testExprs)){
     testExprs <- list(testExprs)
@@ -107,38 +111,74 @@ virtual_ic50 <- function(cellLineEset, drugName, testExprs, seed=2013, reverseDr
   if(reverseDrug){
     drug_vec <- -drug_vec
   }
-  mask <- !is.na(drug_vec)
-  X <- exprs(CL.m[,mask])
-  y = drug_vec[mask]
+  has_drug_mask <- !is.na(drug_vec)
+  X <- exprs(CL.m[,has_drug_mask])
+  y = drug_vec[has_drug_mask]
+  tissueTypes <- getTissueType(cellLineEset)[has_drug_mask]
+  penalty <- rep(1, nrow(X))
+  if(diseaseOffset){
+    mm <- model.matrix(~ -1 + factor(tissueTypes))
+    X <- rbind(X, t(mm))
+    penalty <- c(penalty, rep(0, ncol(mm)))
+  }
   
-  cv <- cv.glmnet(t(X),y,alpha=.1,nfolds=5)
+  obsWeights <- rep(1, length(y))
+  if(!is.null(diseaseUpweight)){
+    diseaseMask <- tissueTypes %in% diseaseUpweight
+    if(sum(diseaseMask) == 0){ stop(paste("'",diseaseUpweight, "' not a valid disease",sep=""))}
+    # weight disease specific to have 1/3 of all weight
+    obsWeights[!diseaseMask] <- 1 * sum(diseaseMask) / sum(!diseaseMask)
+  }
+  #browser()
+  cv <- cv.glmnet(t(X),y,alpha=alpha,nfolds=5,weights=obsWeights,penalty.factor=penalty)
   fits <- mclapply(1:num.bootstraps, function(i){
     N <- length(y)
     idxs <- sample(N,replace=TRUE)
-    fit <- glmnet(t(X[,idxs]),y[idxs],alpha=.1,lambda=cv$lambda.min)
+    fit <- glmnet(t(X[,idxs]),y[idxs],alpha=alpha,lambda=cv$lambda.min, weights=obsWeights[idxs], penalty.factor=penalty)
     y_hat <- rep(NA, N)
     y_hat[-idxs] <- predict(fit, t(X[,-idxs]))
     
     list(fit=fit,y_hat=y_hat)
-  },mc.cores=3,mc.set.seed=TRUE)
+  },mc.cores=ncores,mc.set.seed=TRUE,mc.preschedule=FALSE)
+  
   
   ############
   ## evaluate performance in cell lines
-  
+  perf=NA
+  perfList <- list()
   if(perf.eval){
+    
     Ymatrix <- do.call("rbind", lapply(fits, function(x) x$y_hat))
     Ymean <- colMeans(Ymatrix, na.rm=TRUE)
-    Ymed <- apply(Ymatrix, 2, median, na.rm=TRUE)
+    
     na.mask <- !is.na(Ymean)
     Ymean <- Ymean[na.mask]
     y <- y[na.mask]
+    tt <- tissueTypes[na.mask]
+    ttTbl <- table(tt)
+    diseaseTests <- c("ALL",names(ttTbl)[ttTbl >= 10])
     
-    q <- quantile(y, c(.25, .75))
-    mask <- y <= q[1] | y >= q[2]
-    pred <- prediction(Ymean[mask], factor(y[mask] >= q[2]))
-    perf <- performance(pred, 'auc')@y.values[[1]]
-    plot(performance(pred, 'tpr','fpr'),main=paste(drugName, " (n=",length(y),")",sep=""))
-    text(.4, .4, labels=paste("AUC=",format(perf,digits=2),sep=""),cex=.7,pos=4)
+   
+    par(mfrow=c(ceiling(length(diseaseTests) / 5),5))
+    for(disease in diseaseTests){
+      if(disease == "ALL"){
+        diseaseMask <- rep(TRUE, length(y))
+      }else{
+        diseaseMask <- tt %in% disease
+      }
+      
+      cat(disease,"\n")
+      yDisease <- y[diseaseMask]
+      yDiseaseMean <- Ymean[diseaseMask]
+      q <- quantile(yDisease, c(.40, .60))
+      mask <- yDisease <= q[1] | yDisease >= q[2]
+      #browser()
+      pred <- prediction(yDiseaseMean[mask], factor(yDisease[mask] >= q[2]))
+      perf <- performance(pred, 'auc')@y.values[[1]]
+      perfList[[disease]] <- perf
+      plot(performance(pred, 'tpr','fpr'),main=paste(disease," (n=",length(yDisease),")",sep=""),cex=.8)
+      text(.4, .4, labels=paste("AUC=",format(perf,digits=2),sep=""),cex=.5,pos=4)
+    }
   }
   ###########
   ## apply models on new data
@@ -147,18 +187,26 @@ virtual_ic50 <- function(cellLineEset, drugName, testExprs, seed=2013, reverseDr
   
   y_hats <- lapply(testExprs, function(geneExpr){
     geneExpr.scaled <- normalize_to_X(m, sd, geneExpr)
+    if(diseaseOffset){
+      lvls <- levels(factor(tissueTypes))
+      mm <- matrix(0, nrow=length(lvls), ncol=ncol(geneExpr), dimnames=list(lvls, colnames(geneExpr)))
+      geneExpr.scaled <- rbind(geneExpr.scaled, mm)
+    }
   
     y_hats <- sapply(fits, function(x){
       predict(x$fit, t(geneExpr.scaled))
     })
-    y_hat <- rowMeans(y_hats)
-    names(y_hat) <- colnames(geneExpr)
-    y_hat
+   
+    y_mean <- rowMeans(y_hats)
+    y_sd <- apply(y_hats, 1, sd)
+    names(y_mean) <- colnames(geneExpr)
+    names(y_sd) <- colnames(geneExpr)
+    return (list(y_mean=y_mean, y_sd=y_sd))
   })
   if(length(y_hats)==1){
-    return(y_hats[[1]])
+    return(list(yhat=y_hats[[1]]$y_mean,y_sd = y_hats[[1]]$y_sd, perf=perfList))
   }else{
-    return (y_hats)
+    return (list(yhat=y_hats,perf=perfList))
   }                 
 }
 
@@ -243,83 +291,104 @@ find_drug_features <- function(drugvec,
                                discreteFeatureMask,
                                beta_threshold=10^-3,
                                num.bootstraps=50,
-                               method="lasso"){
+                               method="lasso",
+                               ncores=5){
   
   #####
+ # browser()
   idxs <- groupMatch(names(drugvec), colnames(featureMat))
   y_hat.m <- drugvec[idxs[[1]]]
   A <- featureMat[, idxs[[2]]]
-  cat("Number samples = ", length(y_hat.m),"\n")
+  N <- length(y_hat.m)
+  cat("Number samples = ", N,"\n")
   cat("Number features = ", nrow(A),"\n")
 
   pvals <- rep(NA, nrow(A))
-  pvals[discreteFeatureMask] <- apply(A[discreteFeatureMask,], 1, function(x){ wilcox.test(y_hat.m ~ factor(x))$p.value})
+  pvals[discreteFeatureMask] <- apply(A[discreteFeatureMask,], 1, function(x){ 
+    f <- factor(x)
+    pval <- ifelse(length(levels(f)) < 2, 1, wilcox.test(y_hat.m ~ f)$p.value)
+    pval
+  })
   if(sum(!discreteFeatureMask) > 0){
     pvals[!discreteFeatureMask] <- apply(A[!discreteFeatureMask,,drop=FALSE], 1, function(x){ cor.test(x, y_hat.m,method="spearman")$p.value})
   }
-  if(method=='lasso'){
-    N <- length(y_hat.m)
-    fits <- mclapply(1:num.bootstraps, function(i){
-      idxs <- sample(N,replace=TRUE)
-      cv.fit <- cv.glmnet(t(A)[idxs,], y_hat.m[idxs], alpha=.99,nfolds=5)
-      fit <- glmnet(t(A)[idxs,], y_hat.m[idxs], lambda=cv.fit$lambda.1se,alpha=.99)
-      
-      y_hat <- rep(NA, N)
-      y_hat[-idxs] <- predict(fit, t(A[,-idxs]))
-      
-      list(fit=fit,y_hat=y_hat)
-    },mc.cores=min(num.bootstraps,10),mc.set.seed=TRUE)
+  fits <- mclapply(1:num.bootstraps, function(i){
+    idxs <- sample(N,replace=TRUE)
+    X <- t(A)
+    cv.fit <- cv.glmnet(X[idxs,], y_hat.m[idxs], alpha=.9,nfolds=5)
+    fit <- glmnet(X[idxs,], y_hat.m[idxs], lambda=cv.fit$lambda.min,alpha=.9)
     
-    # evaluate performance
-    Ymatrix <- do.call("rbind", lapply(fits, function(x) x$y_hat))
-    Ymean <- colMeans(Ymatrix, na.rm=TRUE)
-    Ymed <- apply(Ymatrix, 2, mean, na.rm=TRUE)
+    y_hat <- rep(NA, N)
+    y_hat[-idxs] <- predict(fit, X[-idxs,])
     
-    na.check <- is.na(Ymean)
-    if(any(na.check)){
-      warning("NAs in Ymean\n")
-      Ymean <- Ymean[!na.check]
-      y_hat.m <- y_hat.m[!na.check]
-    }
-    
-    q <- quantile(y_hat.m, c(.25, .75))
-    mask <- y_hat.m <= q[1] | y_hat.m >= q[2]
-    pred <- prediction(Ymean[mask], factor(y_hat.m[mask] >= q[2]))
-    perf <- performance(pred, 'auc')@y.values[[1]]
-    rho <- cor(Ymean, y_hat.m,method="spearman")
-    plot(performance(pred, 'tpr','fpr'))
-    text(.4, .4, labels=paste("AUC=",format(perf,digits=2),sep=""),cex=.7,pos=4)
-    text(.4, .3, labels=paste("R=",format(rho,digits=2),sep=""),cex=.7,pos=4)
-    
-   
-    R <- do.call("cbind", lapply(fits, function(x) abs(as.numeric(x$fit$beta)) > beta_threshold))
-    R.plus <- rowSums(do.call("cbind", lapply(fits, function(x) as.numeric(x$fit$beta) > 0)))
-    R.neg <- rowSums(do.call("cbind", lapply(fits, function(x) as.numeric(x$fit$beta) < 0)))
-    pos_freq <- R.plus / (R.plus + R.neg)
-    idxs <- order(rowSums(R),decreasing=T)
-    
-    E <- rowMeans(do.call("cbind", lapply(fits, function(x) as.numeric(x$fit$beta))))
-    E <- E * apply(A, 1, sd)
-    
-  }else if(method=="rf"){
-    rf <- randomForest(x=t(A), y=y_hat.m)
-    browser()
-    imp <- importance(rf)
-    idxs <- order(imp, decreasing=TRUE)
-  }else{
-    stop("Bad method")
+    list(fit=fit,y_hat=y_hat)
+  },mc.cores=min(num.bootstraps,ncores),mc.set.seed=TRUE,mc.preschedule=FALSE)
+  
+  #############################
+  #### random features
+  lambda <- cv.glmnet(t(A), y_hat.m, alpha=.9,nfolds=5)$lambda.min
+  R <- A
+  rownames(R) <- paste("rand_",rownames(A),sep="")
+ 
+  foo <- sapply(1:100, function(i){
+    X <- t(rbind(A, R[, sample(N)]))
+    fit <- glmnet(X, y_hat.m, lambda=lambda,alpha=.9)
+    rand_mask <- grepl("^rand_", colnames(X))
+    noRand <- sum(abs(as.numeric(fit$beta))[rand_mask] > beta_threshold)
+    noObs <- sum(abs(as.numeric(fit$beta))[!rand_mask] > beta_threshold)
+    (noRand + 1) / (noObs + noRand + 1)
+  })
+  fdr <- exp(mean(log(foo)))
+  
+  #############################
+  ## performance assessment
+  Ymatrix <- do.call("rbind", lapply(fits, function(x) x$y_hat))
+  Ymean <- colMeans(Ymatrix, na.rm=TRUE)
+  Ymed <- apply(Ymatrix, 2, mean, na.rm=TRUE)
+  
+  na.check <- is.na(Ymean)
+  if(any(na.check)){
+    warning("NAs in Ymean\n")
+    Ymean <- Ymean[!na.check]
+    y_hat.m <- y_hat.m[!na.check]
   }
+
+  q <- quantile(y_hat.m, c(.25, .75))
+  mask <- y_hat.m <= q[1] | y_hat.m >= q[2]
+  pred <- prediction(Ymean[mask], factor(y_hat.m[mask] >= q[2]))
+  perf <- performance(pred, 'auc')@y.values[[1]]
+  rho <- cor(Ymean, y_hat.m,method="spearman")
+  par(mfrow=c(1,1))
+  plot(performance(pred, 'tpr','fpr'))
+  text(.4, .4, labels=paste("AUC=",format(perf,digits=2),sep=""),cex=.7,pos=4)
+  text(.4, .3, labels=paste("R=",format(rho,digits=2),sep=""),cex=.7,pos=4)
+  
+  ##########
+  
+ 
+  R <- do.call("cbind", lapply(fits, function(x) abs(as.numeric(x$fit$beta)) > beta_threshold))
+ 
+  R.plus <- rowSums(do.call("cbind", lapply(fits, function(x) as.numeric(x$fit$beta) > 0)))
+  R.neg <- rowSums(do.call("cbind", lapply(fits, function(x) as.numeric(x$fit$beta) < 0)))
+  pos_freq <- R.plus / (R.plus + R.neg)
+  idxs <- order(rowSums(R),decreasing=T)
+  #browser()
+  E <- rowMeans(do.call("cbind", lapply(fits, function(x) as.numeric(x$fit$beta))))
+  E <- E * apply(A, 1, sd)
+    
+ 
   abberationCount <- rowSums(A)[idxs]
   abberationCount[grepl("^prot",names(abberationCount))] <- NA
   
   tmp <- data.frame(genes=rownames(A)[idxs], 
                     counts=rowSums(R)[idxs],
                     posFreq=pos_freq[idxs],
+                    effect=E[idxs],
                     freqCounts=rowSums(R)[idxs]/num.bootstraps,
                     noEvents=abberationCount,
                     freqEvents=abberationCount/N,
                     pvals=pvals[idxs])
-  return (list(df=tmp,N=N,metric=c(rho=rho,auc=perf),effect=E,dataMatrix=A,fits=lapply(fits, function(x) x$fit)))
+  return (list(df=tmp,N=N,metric=c(rho=rho,auc=perf),fdr=fdr,dataMatrix=A,fits=lapply(fits, function(x) x$fit)))
 }
 
 
@@ -389,34 +458,34 @@ ccleModelApply <- function(fgf, drugVector){
 #   mutM
 # }
 
-plot_features <- function(F,drug, disease,top=25,text.cex=.7){
-  DF <- F$df[1:top,]
-  N <- nrow(DF)
-  sz <- DF$freqEvents * 20
-  pch <- rep(21,N)
-  pch[is.na(sz)] <- 22
-  sz[is.na(sz)] <- 1
-  col <- apply(col2rgb(rainbow(N)), 2, function(x) {rgb(x[1]/255,x[2]/255,x[3]/255,.5)})
-  
-  DF$pvals[DF$pvals < 10^-20] <- 10^-20
-
-  x <- DF$freqCounts + runif(length(DF$freqCounts), 0, .1) -.05
-  y <- -log10(DF$pvals)
-  par(mar=c(5,5, 2,15))
-  plot(x, y,pch=pch,bg=col,cex=sz,xlim=c(min(x)-.1, max(x)+.1),ylim=c(0, max(y)+1),
-       ylab="-log10(pval)",xlab="Freq of selection",main=paste(drug," in ", disease," (n=",F$N,")",sep=""))
-  #mtext(paste("n=",num.samples,sep=""),3)
-  par(xpd=TRUE)
-  
-  text.col <- c("blue","black","red")[cut(DF$posFreq,breaks=c(0,.3,.7,1),include.lowest=TRUE)]
-  tmp <- legend(par()$usr[2]+.03,par()$usr[4],
-                legend=paste(DF$genes," (",format(DF$noEvents/F$N * 100,digits=1,trim=TRUE),"%)",sep=""),
-                pch=pch,pt.bg=col,cex=text.cex,xjust=0,text.col=text.col)
-  for(i in 1:N){
-    x.line <- tmp$rect$left + (tmp$text$x[i] - tmp$rect$left)/2
-    lines(x=c(x.line,x[i]),y=c(tmp$text$y[i],y[i]),lwd=.5,col="gray")
-  }
-}
+# plot_features <- function(F,drug, disease,top=25,text.cex=.7){
+#   DF <- F$df[1:top,]
+#   N <- nrow(DF)
+#   sz <- DF$freqEvents * 20
+#   pch <- rep(21,N)
+#   pch[is.na(sz)] <- 22
+#   sz[is.na(sz)] <- 1
+#   col <- apply(col2rgb(rainbow(N)), 2, function(x) {rgb(x[1]/255,x[2]/255,x[3]/255,.5)})
+#   
+#   DF$pvals[DF$pvals < 10^-20] <- 10^-20
+# 
+#   x <- DF$freqCounts + runif(length(DF$freqCounts), 0, .1) -.05
+#   y <- -log10(DF$pvals)
+#   par(mar=c(5,5, 2,15))
+#   plot(x, y,pch=pch,bg=col,cex=sz,xlim=c(min(x)-.1, max(x)+.1),ylim=c(0, max(y)+1),
+#        ylab="-log10(pval)",xlab="Freq of selection",main=paste(drug," in ", disease," (n=",F$N,")",sep=""))
+#   #mtext(paste("n=",num.samples,sep=""),3)
+#   par(xpd=TRUE)
+#   
+#   text.col <- c("blue","black","red")[cut(DF$posFreq,breaks=c(0,.3,.7,1),include.lowest=TRUE)]
+#   tmp <- legend(par()$usr[2]+.03,par()$usr[4],
+#                 legend=paste(DF$genes," (",format(DF$noEvents/F$N * 100,digits=1,trim=TRUE),"%)",sep=""),
+#                 pch=pch,pt.bg=col,cex=text.cex,xjust=0,text.col=text.col)
+#   for(i in 1:N){
+#     x.line <- tmp$rect$left + (tmp$text$x[i] - tmp$rect$left)/2
+#     lines(x=c(x.line,x[i]),y=c(tmp$text$y[i],y[i]),lwd=.5,col="gray")
+#   }
+# }
 
 plot_features_2 <- function(F,title,effectPlot=FALSE,top=25,text.cex=.7,bubble.cex=1.5){
   DF <- F$df[1:top,]
@@ -434,7 +503,7 @@ plot_features_2 <- function(F,title,effectPlot=FALSE,top=25,text.cex=.7,bubble.c
   y <- -log10(DF$pvals)
   par(mar=c(5,5, 2,15))
   plot(x, y,pch=pch,col=col,cex=sqrt(sz) * bubble.cex,
-       xlim=c(min(x)-.1, max(x)+.1),
+       xlim=c(0,1),
        ylim=c(0, max(y)+1),
        yaxt="n",
        ylab="Univariate significance",
@@ -466,6 +535,63 @@ plot_features_2 <- function(F,title,effectPlot=FALSE,top=25,text.cex=.7,bubble.c
   tmp3 <- legend(par()$usr[2]+.03 + tmp$rect$w + tmp2$rect$w, y=par()$usr[4], 
                  legend=assocText, cex=text.cex,xjust=0,text.col=text.col,bty="n",
                  title="Assoc.",title.col="black")
+}
+
+plot_features_3 <- function(F,title,top=25,text.cex=.7,bubble.cex=1.5){
+  DF <- F$df[1:top,]
+  N <- nrow(DF)
+  sz <- DF$freqEvents * 20
+  pch <- rep(19,N)
+  #pch[is.na(sz)] <- 19
+  sz[is.na(sz)] <- 1
+  col <- apply(col2rgb(rainbow(N)), 2, function(x) {rgb(x[1]/255,x[2]/255,x[3]/255,.5)})
+  #col <- col[sample(N)]
+  
+  DF$pvals[DF$pvals < 10^-20] <- 10^-20
+  
+  y <- DF$freqCounts + runif(length(DF$freqCounts), 0, .05) -.05
+  x <- DF$effect
+  par(mar=c(5,5, 2,15))
+  plot(x, y,pch=pch,col=col,cex=sqrt(sz) * bubble.cex,
+       ylim=c(0,1),
+       #yaxt="n",
+       xlab="Effect size",
+       ylab="Importance score\n(# times selected / 100 bootstraps)",main=title)
+  abline(v=0)
+  #mtext(paste("n=",num.samples,sep=""),3)
+  #at.axis <- seq(0, max(y)+1,by=3)
+  #axis.lbl <- parse(text=paste("10^-",at.axis,sep=""))
+  #axis(side=2, at=at.axis,labels=axis.lbl,las=2)
+  par(xpd=TRUE)
+  
+  text.col <- c("dodgerblue4","black","firebrick")[cut(DF$posFreq,breaks=c(0,.3,.7,1),include.lowest=TRUE)]
+  assocText <- c("neg","?","pos")[cut(DF$posFreq,breaks=c(0,.3,.7,1),include.lowest=TRUE)]
+  freqText <- paste(format(DF$noEvents/F$N * 100,digits=1,justify="right",width=3),"%",sep="")
+  #browser()
+  tmp <- legend(par()$usr[2],par()$usr[4],
+                legend=as.character(DF$genes),
+                pch=21,pt.bg=col,cex=text.cex,xjust=0,text.col=text.col,bty="n",
+                title="Aberration",title.col="black")
+  
+  for(i in 1:N){
+    x.line <- tmp$rect$left + (tmp$text$x[i] - tmp$rect$left)/2
+    lines(x=c(x.line,x[i]),y=c(tmp$text$y[i],y[i]),lwd=.5,col=col[i])
+  }
+  #browser()
+  tmp2 <- legend(par()$usr[2]+.03 +tmp$rect$w, y=par()$usr[4], 
+                 legend=freqText, cex=text.cex,xjust=0,text.col=text.col,bty="n",
+                 title="Freq.",title.col="black")
+  
+  #tmp3 <- legend(par()$usr[2]+.03 + tmp$rect$w + tmp2$rect$w, y=par()$usr[4], 
+  #               legend=assocText, cex=text.cex,xjust=0,text.col=text.col,bty="n",
+  #               title="Assoc.",title.col="black")
+}
+
+falsediscovery_plot <- function(df){
+  N <- nrow(df)
+  is.rand <- grepl("^rand_", rownames(df))
+  cs <- cumsum(is.rand)
+  fdisc <- (cs / 1:N) * 2
 }
 
 
